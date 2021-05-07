d5b2442189fba93c4393720be4cbac6627e15bc7
demetrius <back2musicshop@gmail.com>
2021-05-07 19:30:35 +0300

    level 2 1-2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+
+fetch(API_URL);
+fetch(API_URL)
+  .then(response => response.json())
+  .then(file => {
+    const fighters = JSON.parse(atob(file.content));
+    const names = fighters.map(it => it.name).join('\n');
+    console.log(fighters);
+  });
 
