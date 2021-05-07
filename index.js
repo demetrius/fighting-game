466ada5f6fe665a3f07b1fb7e0385b1367e61888
demetrius <back2musicshop@gmail.com>
2021-05-07 20:19:14 +0300

    level 2-4

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
 
-rootElement.innerText = 'Loading...';
 
 fetch(API_URL);
 fetch(API_URL)
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      Promise.reject(new Error('Failed load data'));
+    }
+  return response.json();
+  })
   .then(file => {
     const fighters = JSON.parse(atob(file.content));
     const names = fighters.map(it => it.name).join('\n');
     
     rootElement.innerText = names;
+    loadingElement.remove();
 
+  })
+
+  .catch(error => {
+    console.warn(error);
+    root.innerText = 'Failed to load data';
+    loadingElement.remove();
   });
 
